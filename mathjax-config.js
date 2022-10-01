9ef8f9c0b9bf1ecd3ab01a448a6a891e3ba811ed
Shibaken28 <8128kanzensu@gmail.com>
2022-10-01 23:13:55 +0900

    feat: add CakeCTF2021

---
+++ mathjax-config.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    MathJax.Hub.Config({
+        tex2jax: {inlineMath: [['$','$']]}
+    });
+    MathJax.Hub.Config({
+        tex2jax: {displayMath: [['$$','$$']]}
+    });
+}
